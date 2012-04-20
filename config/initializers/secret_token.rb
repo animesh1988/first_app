62889230ff2138d776739dad579a2c6fea5f1e48
animesh <animesh@ubuntu.ubuntu-domain>
2012-04-20 23:28:25 +0530

    Initial Commit - Animesh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '97c1b19b47a8a4f9d26ffb8198c3de02eeafbe2ddba714b85cd223e497b512460e6c787b58e0d9bb33d4404b2aed37984512ac232a1528b0286ed15c16eaf7e1'
